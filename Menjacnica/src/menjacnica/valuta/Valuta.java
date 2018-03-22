26611e49feb1ecf419cd0d1986d9cd1a0b9b4906
Teodora Acimov <acimov.teodora@gmail.com>
2018-03-22 12:11:54 +0100

    Napravljena klasa Valuta sa svojim aributima.

---
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -0,0 +1,50 @@
+package menjacnica.valuta;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	private String naziv;
+	private String skracenica;
+	private GregorianCalendar datum;
+	private double prodajniKurs;
+	private double kupovniKurs;
+	private double srednjiKurs;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkracenica() {
+		return skracenica;
+	}
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	
+}
