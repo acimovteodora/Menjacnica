0fdf1b2a68488dba06185307073f8fd0c659ced6
Teodora Acimov <acimov.teodora@gmail.com>
2018-03-22 12:29:35 +0100

    Generisane metode equals, hashCode i  toString.

--- Menjacnica/src/menjacnica/valuta/Valuta.java
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -46,5 +46,47 @@ public class Valuta {
 	public void setSrednjiKurs(double srednjiKurs) {
 		this.srednjiKurs = srednjiKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta: " + naziv + " (" + skracenica + ") \n" + datum + "\n\tProdajniKurs="
+				+ prodajniKurs + "\n\tKupovniKurs=" + kupovniKurs + "\n\tSrednjiKurs=" + srednjiKurs ;
+	}
+		
+	
 	
 }
